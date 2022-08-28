c840d0cf3b5d3bc7ff442aa59880e4240920eff3
Ranaihab <rana.ihab7@gmail.com>
2022-08-28 04:46:31 +0200

    Adding modes, admin, swagger

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+Calculator function
+"""
+
+
+def add(x, y):
+    return x + y
+
+
+def subtarct(x, y):
+    return y - x
---
+++ app/core/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+
+# Create your models here.
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra):
+        if not email:
+            raise ValueError('Email id blank')
+        user = self.model(email=self.normalize_email(email), **extra)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra):
+        user = self.create_user(email, password, **extra)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager() # assign user manager to the user class
+
+    USERNAME_FIELD = 'email' # USERNAME_FIELD sets the field that we use for authentication
